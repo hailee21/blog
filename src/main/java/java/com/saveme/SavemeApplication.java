0e077f24e98500fe514ff7585e7b03a5e107024e
Hailee <haileeyu21@gmail.com>
2021-03-19 23:43:33 +0900

    [하람] first commit

---
+++ src/main/java/java/com/saveme/SavemeApplication.java
@@ -0,0 +1,13 @@
+package java.com.saveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemeApplication.class, args);
+	}
+
+}
