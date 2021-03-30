39f7ab1473cc9c369d7c82ca76baf47289f700fd
Hailee <haileeyu21@gmail.com>
2021-03-31 03:22:12 +0900

    [하람] boardList 조회 시 comments 개수 표출

--- src/main/java/com/test/board/repository/BoardRepository.java
+++ src/main/java/com/test/board/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.test.board.repository;
 
 import com.test.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,14 @@ import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
+    //List<BoardEntity> findAllByOrderByIdAsc();
+
+    @Query(value = "SELECT b.*,\n" +
+            "       COUNT(c.content) AS comment_num\n" +
+            "FROM boards b\n" +
+            "LEFT JOIN comments c ON b.board_id = c.board_id\n" +
+            "GROUP BY b.board_id\n" +
+            "ORDER BY b.board_id",
+            nativeQuery = true)
     List<BoardEntity> findAllByOrderByIdAsc();
 }
